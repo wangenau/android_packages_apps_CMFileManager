b696f75f44efe7c4ef7bdcd5f255b21d8166b5d4
Wanja Schulze <rajaschu@googlemail.com>
2018-02-19 18:37:22 +0100

    Remove exit warning

--- src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
+++ src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
@@ -1923,19 +1923,6 @@ public class NavigationActivity extends Activity
 
         this.mExitFlag = !back();
 
-        // Retrieve if the exit status timeout has expired
-        long now = System.currentTimeMillis();
-        boolean timeout = (this.mExitBackTimeout == -1 ||
-                            (now - this.mExitBackTimeout) > RELEASE_EXIT_CHECK_TIMEOUT);
-
-        //Check if there no history and if the user was advised in the last back action
-        if (this.mExitFlag && (this.mExitFlag != flag || timeout)) {
-            //Communicate the user that the next time the application will be closed
-            this.mExitBackTimeout = System.currentTimeMillis();
-            DialogHelper.showToast(this, R.string.msgs_push_again_to_exit, Toast.LENGTH_SHORT);
-            return true;
-        }
-
         //Back action not applied
         return !this.mExitFlag;
     }
